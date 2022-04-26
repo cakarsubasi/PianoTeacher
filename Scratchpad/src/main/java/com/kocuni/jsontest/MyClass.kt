3e13589609cb215a6e6dc727b9d26114a733688e
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-04-26 20:09:14 +0300

    Added a scratchpad module for running code on PC

---
+++ Scratchpad/src/main/java/com/kocuni/jsontest/MyClass.kt
@@ -0,0 +1,10 @@
+package com.kocuni.jsontest
+
+class MyClass {
+    var x: Int = 0
+}
+
+fun main() {
+    print("Hello world")
+
+}
---
+++ app/src/test/java/com/kocuni/pianoteacher/JSONTest.kt
@@ -0,0 +1,11 @@
+package com.kocuni.pianoteacher
+
+import org.json.*
+import org.junit.Test
+
+class JSONTest {
+
+
+
+
+}
