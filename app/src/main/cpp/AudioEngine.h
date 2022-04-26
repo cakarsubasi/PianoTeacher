015f459453477096e14439dc3e84d519301dbc9b
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-04-26 10:43:58 +0300

    Initial commit.

---
+++ app/src/main/cpp/AudioEngine.h
@@ -0,0 +1,10 @@
+//
+// Created by explo on 2022-04-26.
+//
+
+#ifndef PIANO_TEACHER_AUDIOENGINE_H
+#define PIANO_TEACHER_AUDIOENGINE_H
+
+#include <oboe.oboe.h>
+
+#endif //PIANO_TEACHER_AUDIOENGINE_H
