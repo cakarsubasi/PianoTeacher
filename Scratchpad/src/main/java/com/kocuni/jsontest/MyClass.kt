587007865d588311c65a4c1eb6d4fa38d0ff21c3
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-04-30 20:19:48 +0300

    Scratchpad: wrote the song json parser

--- Scratchpad/src/main/java/com/kocuni/jsontest/MyClass.kt
+++ Scratchpad/src/main/java/com/kocuni/jsontest/MyClass.kt
@@ -3,11 +3,7 @@ package com.kocuni.jsontest
 import org.json.JSONObject
 import java.io.File
 import java.io.FileReader
-import java.io.Reader
-
-class MyClass {
-    var x: Int = 0
-}
+import java.lang.String.format
 
 fun main() {
     println("Hello world")
@@ -20,8 +16,71 @@ fun main() {
     val jsonstr: String = String(buffer)
     println(jsonstr)
 
-    val abstractSong: JSONObject = JSONObject(jsonstr)
+    val abstractSong = JSONObject(jsonstr)
 
-    println(abstractSong["type"])
+    println(abstractSong.toString(4))
+
+    val songObjects = abstractSong.getJSONObject("objects")
+
+    //println(songObjects.getJSONObject("0").getJSONObject("objects").getJSONObject("measures"))
+    val song: Song = Song()
+    val systems = ArrayList<SystemStaff>()
+    for (systemIndex in songObjects.keys()) {
+        val jSystem: JSONObject = songObjects.getJSONObject(systemIndex).getJSONObject("objects")
+        for (index in jSystem.keys()) {
+            // measures, boundaries, staffs
+            //println(format("%s : %s", index, jSystem[index]));
+            val ymin: Double = jSystem.getJSONObject("boundaries").getDouble("ymin")
+            val ymax: Double = jSystem.getJSONObject("boundaries").getDouble("ymax")
+            val jStaffs = jSystem.getJSONObject("staffs")
+            val jMeasures = jSystem.getJSONObject("measures")
+
+            val staffs = ArrayList<Staff>()
+            for (staffIndex in jStaffs.keys()) {
+                val top: Double = jStaffs.getJSONObject(staffIndex).getJSONObject("objects").getDouble("top")
+                val bottom: Double = jStaffs.getJSONObject(staffIndex).getJSONObject("objects").getDouble("bottom")
+                // construct staffs
+                val staff = Staff()
+                staff.top = top
+                staff.bottom = bottom
+                staffs.add(staff)
+            }
+            val measures = ArrayList<Measure>()
+            for (measureIndex in jMeasures.keys()) {
+                // each measure
+                val jMeasureObjs = jMeasures.getJSONObject(measureIndex).getJSONObject("objects")
+
+                val glyphs = ArrayList<Glyph>()
+                for (noteIndex in jMeasureObjs.keys()) {
+                    val jNoteObject = jMeasureObjs.getJSONObject(noteIndex)
+                    val glyphType = jNoteObject.getString("type")
+                    val x = jNoteObject.getJSONObject("objects").getDouble("x")
+                    val y = jNoteObject.getJSONObject("objects").getDouble("y")
+                    val h = jNoteObject.getJSONObject("objects").getDouble("h")
+                    val w = jNoteObject.getJSONObject("objects").getDouble("w")
+
+                    // TODO: typed glyphs
+                    val glyph = Glyph(x, y, h, w)
+                    glyphs.add(glyph)
+                }
+                // construct measures
+                val measure = Measure()
+                measure.glyphs = glyphs
+            }
+
+            // construct system
+            val system = SystemStaff()
+            system.measures = measures
+            system.staffs = staffs
+            system.ymax = ymax
+            system.ymin = ymin
+            systems.add(system)
+        }
+        song.systems = systems
+    }
+
+}
+
+fun constructSong(songjson: JSONObject) {
 
 }
--- Scratchpad/src/main/java/com/kocuni/jsontest/Song.kt
+++ Scratchpad/src/main/java/com/kocuni/jsontest/Song.kt
@@ -1,5 +1,6 @@
 package com.kocuni.jsontest
 
+import javax.lang.model.type.UnionType
 
 class Song {
     /**
@@ -36,12 +37,81 @@ class Song {
      *   Utils
      *     FT/Spectrogram
      */
+    var systems: List<SystemStaff> = ArrayList<SystemStaff>()
+}
 
+class SystemStaff {
+    var ymax: Double = 0.0
+    var ymin: Double = 0.0
 
+    var measures: List<Measure> = ArrayList<Measure>()
+    var staffs: List<Staff> = ArrayList<Staff>();
+}
 
-    companion object {
-        fun constructSong() {
+class Staff {
+    var top: Double = 0.0
+    var bottom: Double = 0.0
+}
 
-        }
+class Measure {
+    var glyphs: List<Glyph> = ArrayList<Glyph>()
+}
+
+open class Glyph(val x: Double = 0.0,
+                 val y: Double = 0.0,
+                 val h: Double = 0.0,
+                 val w: Double = 0.0) : Comparable<Glyph> {
+    override fun compareTo(other: Glyph): Int {
+        return this.x.compareTo(other.x);
     }
+}
+
+class GlyphNote(x: Double = 0.0,
+                y: Double = 0.0,
+                h: Double = 0.0,
+                w: Double = 0.0) : Glyph(x, y, h, w) {
+            val length: Double = 1.0
+}
+
+class GlyphAccidental(x: Double = 0.0,
+                      y: Double = 0.0,
+                      h: Double = 0.0,
+                      w: Double = 0.0) : Glyph(x, y, h, w) {
+            val type: String = "Natural"
+}
+
+class GlyphClef(x: Double = 0.0,
+           y: Double = 0.0,
+           h: Double = 0.0,
+           w: Double = 0.0) : Glyph(x, y, h, w) {
+                    val type: String = "gClef"
+}
+
+class SongStream() {
+
+}
+
+/**
+ * A list of notes that must be played at
+ * the same time.
+ */
+class Chord() {
+
+    data class Pitch(val pitch: Double, val name: String = "C4") { }
+    val notes: ArrayList<Pitch> = ArrayList<Pitch>()
+
+}
+
+class Note(){
+    val pitch: Double = 0.0
+    val name: String = "C4"
+}
+
+enum class Clef {
+    GCLEF, FCLEF, CCLEF
+    //G4 ,   F3 ,   C4
+    // Unfortunately, the meaning
+    // of clefs is dependent on their
+    // position which itself is difficult
+    // to analyze
 }
