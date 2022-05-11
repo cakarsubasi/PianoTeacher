6eacde855c5ec0f8c213e4986abfaa8c786c7fa8
akucukoduk16 <abdullahkucukodu@gmail.com>
2022-05-11 07:10:25 +0300

    Some exception handling

--- app/src/main/java/com/kocuni/pianoteacher/ServerActivity.java
+++ app/src/main/java/com/kocuni/pianoteacher/ServerActivity.java
@@ -1,9 +1,11 @@
 package com.kocuni.pianoteacher;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,6 +16,7 @@ import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,9 +27,14 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -38,6 +46,15 @@ import okhttp3.Response;
 
 public class ServerActivity extends AppCompatActivity {
     String selectedImagePath;
+    private static final Pattern IP_ADDRESS
+            = Pattern.compile(
+            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+                    + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+                    + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                    + "|[1-9][0-9]|[0-9]))");
+    final int SELECT_MULTIPLE_IMAGES = 1;
+    ArrayList<String> selectedImagesPaths; // Paths of the image(s) selected by the user.
+    boolean imagesSelected = false;
     ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -59,38 +76,84 @@ public class ServerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        ActivityCompat.requestPermissions(ServerActivity.this, new String[]{Manifest.permission.INTERNET}, 2);
+        ActivityCompat.requestPermissions(ServerActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
 
         setContentView(R.layout.activity_server);
     }
-
-     public void connectServer(View v){
-        EditText ipv4AddressView = findViewById(R.id.IPAddress);
-        String ipv4Address = ipv4AddressView.getText().toString();
-        EditText portNumberView = findViewById(R.id.portNumber);
-        String portNumber = portNumberView.getText().toString();
-
-        String postUrl= "http://"+ipv4Address+":"+portNumber+"/";
-
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-        // Read BitMap by file path
-        Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath, options);
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-
-        RequestBody postBodyImage = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
-                .build();
-
-        TextView responseText = findViewById(R.id.responseText);
-        responseText.setText("Please wait ...");
-
-        postRequest(postUrl, postBodyImage);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(getApplicationContext(), "Access to Storage Permission Granted. Thanks.", Toast.LENGTH_SHORT).show();
+                } else {
+//                    Toast.makeText(getApplicationContext(), "Access to Storage Permission Denied.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 2: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(getApplicationContext(), "Access to Internet Permission Granted. Thanks.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Access to Internet Permission Denied.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 
+     public void connectServer(View v){
+         TextView responseText = findViewById(R.id.responseText);
+         if (imagesSelected == false) { // This means no image is selected and thus nothing to upload.
+             responseText.setText("No Image Selected to Upload. Select Image(s) and Try Again.");
+             return;
+         }
+         responseText.setText("Sending the Files. Please Wait ...");
+
+         EditText ipv4AddressView = findViewById(R.id.IPAddress);
+         String ipv4Address = ipv4AddressView.getText().toString();
+         EditText portNumberView = findViewById(R.id.portNumber);
+         String portNumber = portNumberView.getText().toString();
+
+         Matcher matcher = IP_ADDRESS.matcher(ipv4Address);
+         if (!matcher.matches()) {
+             responseText.setText("Invalid IPv4 Address. Please Check Your Inputs.");
+             return;
+         }
+
+         String postUrl = "http://" + ipv4Address + ":" + portNumber + "/";
+
+         MultipartBody.Builder multipartBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+
+         for (int i = 0; i < selectedImagesPaths.size(); i++) {
+             BitmapFactory.Options options = new BitmapFactory.Options();
+             options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+             ByteArrayOutputStream stream = new ByteArrayOutputStream();
+             try {
+                 // Read BitMap by file path.
+                 Bitmap bitmap = BitmapFactory.decodeFile(selectedImagesPaths.get(i), options);
+                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+             }catch(Exception e){
+                 responseText.setText("Please Make Sure the Selected File is an Image.");
+                 return;
+             }
+             byte[] byteArray = stream.toByteArray();
+
+             multipartBodyBuilder.addFormDataPart("image" + i, "Android_Flask_" + i + ".jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray));
+         }
+
+         RequestBody postBodyImage = multipartBodyBuilder.build();
+
+//        RequestBody postBodyImage = new MultipartBody.Builder()
+//                .setType(MultipartBody.FORM)
+//                .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
+//                .build();
+
+         postRequest(postUrl, postBodyImage);
+    }
     void postRequest(String postUrl, RequestBody postBody) {
 
         OkHttpClient client = new OkHttpClient();
@@ -105,13 +168,14 @@ public class ServerActivity extends AppCompatActivity {
             public void onFailure(Call call, IOException e) {
                 // Cancel the post on failure.
                 call.cancel();
+                Log.d("FAIL", e.getMessage());
 
                 // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         TextView responseText = findViewById(R.id.responseText);
-                        responseText.setText("Failed to Connect to Server");
+                        responseText.setText("Failed to Connect to Server. Please Try Again.");
                     }
                 });
             }
@@ -124,7 +188,7 @@ public class ServerActivity extends AppCompatActivity {
                     public void run() {
                         TextView responseText = findViewById(R.id.responseText);
                         try {
-                            responseText.setText(response.body().string());
+                            responseText.setText("Server's Response\n" + response.body().string());
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -133,7 +197,6 @@ public class ServerActivity extends AppCompatActivity {
             }
         });
     }
-
     public void selectImage(View v) {
         Intent intent = new Intent();
         intent.setType("*/*");
