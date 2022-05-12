2a9938210eb8bb074dca2f6c95941a8f5907ce15
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-05-12 17:07:14 +0300

    Sample stream added.

--- app/src/main/java/com/kocuni/pianoteacher/music/SampleSongs.kt
+++ app/src/main/java/com/kocuni/pianoteacher/music/SampleSongs.kt
@@ -7,14 +7,25 @@ package com.kocuni.pianoteacher.music
 object SampleSongs {
 
     fun song1() : Stream {
-
         val chordify = {str: String -> Stream.Chord(Stream.Note(str))}
+        val generatePart = { str: String ->
+            val list: MutableList<Stream.Chord> = mutableListOf()
+            str.splitToSequence(" ").forEach {
+                list.add(chordify(it))
+            }
+            Stream.Part(list)
+        }
 
+        val measure1 = "C4 D4 E4 D4"
+        val measure2 = "A4 C4 D4 A4"
+        val measure3 = "B4 C5 D4 G4"
+        val measure4 = "C4 D4 E4 D4"
 
-        chordify("C4")
-
-
-
-        return Stream(listOf())
+        return Stream(listOf(
+            generatePart(measure1),
+            generatePart(measure2),
+            generatePart(measure3),
+            generatePart(measure4)
+        ))
     }
 }
