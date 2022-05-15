782c402ba79214ad5b719060927961be3fa30b87
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-05-15 14:23:21 +0300

    More UI work.

--- app/src/main/java/com/kocuni/pianoteacher/ui/songselection/SongSelection.kt
+++ app/src/main/java/com/kocuni/pianoteacher/ui/songselection/SongSelection.kt
@@ -1,14 +1,22 @@
 package com.kocuni.pianoteacher.ui.songselection
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,6 +39,7 @@ fun SongList(
     title: String = "Song List",
     songs: List<SongHolder> = listOf(SongHolder()),) {
     Column() {
+        Text(text = title)
         Column() {
             songs.forEach { song ->
                 SongRow(song)
@@ -41,14 +50,24 @@ fun SongList(
 
 @Preview
 @Composable
-fun SongRow(song: SongHolder = SongHolder()) {
-    Row() {
-        Text(song.name)
-        Spacer(modifier = Modifier.height(4.dp))
+fun SongRow(
+    song: SongHolder = SongHolder(),
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = Modifier.clickable(onClick = onClick),
+        )
+    {
+        Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Text( text = song.name,
+                 style = MaterialTheme.typography.body2,
+            )
+        }
     }
+    Spacer(modifier = Modifier.height(4.dp))
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun SongListPreview() {
     val song1 = SongHolder("Hardcoded song 1")
