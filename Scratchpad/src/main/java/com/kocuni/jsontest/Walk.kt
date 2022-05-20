f507af66b3bba8c7f7d67535c322e1c21d5fbbae
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-05-20 21:59:11 +0300

    Initial commit for File reading classes

--- Scratchpad/src/main/java/com/kocuni/jsontest/Walk.kt
+++ Scratchpad/src/main/java/com/kocuni/jsontest/Walk.kt
@@ -2,6 +2,7 @@ package com.kocuni.jsontest
 
 import java.nio.file.Files
 import kotlin.io.path.Path
+import kotlin.io.path.name
 
 class Walk {
 
@@ -10,8 +11,10 @@ class Walk {
 
 fun main() {
 
-    val st = Files.walk(Path(""))
-    st.forEach {
+    val st = Files.walk(Path("Scratchpad"))
+    st.filter {
+        it.name.contains(Regex.fromLiteral(".json"))
+    }.forEach {
         println(it)
     }
 }
--- app/src/main/java/com/kocuni/pianoteacher/utils/FileManager.kt
+++ app/src/main/java/com/kocuni/pianoteacher/utils/FileManager.kt
@@ -1,7 +1,9 @@
 package com.kocuni.pianoteacher.utils
 
+import android.os.Environment
 import com.kocuni.pianoteacher.music.data.TutorableSong
 import org.json.JSONObject
+import java.io.File
 import java.io.InputStream
 import java.lang.NullPointerException
 
@@ -12,16 +14,15 @@ class FileManager {
     companion object {
         fun saveJSON() {
 
+
+
         }
 
         fun loadJSON() {
 
         }
 
-        /**
-         * TODO: There might be an issue with how the songs are parsed
-         * Since I get a lot of F4s on the only test performed.
-         */
+        @Throws(NullPointerException::class)
         fun getSongFromJSONStream(f: InputStream) : TutorableSong {
             val jsonStr = f.bufferedReader().readLine()
             if (jsonStr != null) {
