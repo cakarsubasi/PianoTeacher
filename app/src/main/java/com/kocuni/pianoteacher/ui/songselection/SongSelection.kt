a2b800e9e6e6b65d5b05574fe392facb13d0618b
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-05-22 18:03:23 +0300

    Song Selection fragment now has reasonably sized buttons.

--- app/src/main/java/com/kocuni/pianoteacher/ui/songselection/SongSelection.kt
+++ app/src/main/java/com/kocuni/pianoteacher/ui/songselection/SongSelection.kt
@@ -3,15 +3,9 @@ package com.kocuni.pianoteacher.ui.songselection
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -39,18 +33,59 @@ fun SongSelection(
         viewModel.setName(it.name)
         returnToTutor()
     }
-    LazyColumn {
-        // TODO
-        fileManager.songs.forEach {
-            item {
-                SongRow(
-                    SongHolder(it.name)
-                ) { setSong(it) }
+    Column() {
+        TopAppBar() {
+            Text(text = "Song Select",
+            style = MaterialTheme.typography.h4
+            )
+        }
+        LazyColumn {
+
+            fileManager.songs.forEach {
+                item {
+                    SongRow(
+                        SongHolder(it.name)
+                    ) { setSong(it) }
+                }
+
+                item {
+                    Spacer(
+                        modifier = Modifier.height(10.dp)
+                    )
+                }
             }
         }
     }
+
+
 }
 
+
+@Preview
+@Composable
+fun SongRow(
+    song: SongHolder = SongHolder(),
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = Modifier.clickable(onClick = onClick),
+    )
+    {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+                .padding(horizontal = 4.dp, vertical = 0.dp),
+            shape = MaterialTheme.shapes.medium,
+            elevation = 4.dp) {
+            Text( text = song.name,
+                style = MaterialTheme.typography.h5,
+
+            )
+        }
+    }
+    Spacer(modifier = Modifier.height(4.dp))
+}
 
 @Composable
 fun SongList(
@@ -67,24 +102,7 @@ fun SongList(
     }
 }
 
-@Preview
-@Composable
-fun SongRow(
-    song: SongHolder = SongHolder(),
-    onClick: () -> Unit = {},
-) {
-    Row(
-        modifier = Modifier.clickable(onClick = onClick),
-        )
-    {
-        Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-            Text( text = song.name,
-                 style = MaterialTheme.typography.body2,
-            )
-        }
-    }
-    Spacer(modifier = Modifier.height(4.dp))
-}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
