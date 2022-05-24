2fe3f52b1affb6e5fc00fe9f8517fcda400472e0
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-05-24 12:04:32 +0300

    Removed unused Activity.

--- app/src/main/java/com/kocuni/pianoteacher/ui/music/Tutor.kt
+++ app/src/main/java/com/kocuni/pianoteacher/ui/music/Tutor.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.kocuni.pianoteacher.Greeting
 import com.kocuni.pianoteacher.SongTutorViewModel
 import com.kocuni.pianoteacher.music.SongTutor
 import com.kocuni.pianoteacher.music.data.SampleSongs
@@ -29,7 +28,7 @@ import com.kocuni.pianoteacher.ui.theme.PianoTeacherTheme
 @Composable
 fun DefaultPreview3() {
     PianoTeacherTheme {
-        Greeting("Android")
+
         Column {
             Row {
                 SongMenuBar()
