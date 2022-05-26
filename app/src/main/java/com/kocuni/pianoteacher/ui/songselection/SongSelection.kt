1d642089ad87d65394069f8e43310846a2105bf2
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-05-26 19:34:30 +0300

    Implemented bad file deletion.

--- app/src/main/java/com/kocuni/pianoteacher/ui/songselection/SongSelection.kt
+++ app/src/main/java/com/kocuni/pianoteacher/ui/songselection/SongSelection.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,20 +14,31 @@ import com.kocuni.pianoteacher.SongTutorViewModel
 import com.kocuni.pianoteacher.utils.data.SongFile
 
 
-// TODO
-
 @Composable
 fun SongSelection(
     viewModel: SongTutorViewModel,
     returnToTutor: () -> Unit = {},
 ) {
+    val deletionDialog = remember { mutableStateOf(false) }
+    val delete_this = remember {mutableStateOf({})}
     val fileManager = viewModel.fileManager
     fileManager.readAllFiles()
+
     val setSong : (SongFile) -> Unit = {
         viewModel.setSong(it.getTutorable())
         viewModel.setName(it.name)
         returnToTutor()
     }
+    val deleteSong : (SongFile) -> Unit = {
+        deletionDialog.value = true
+        delete_this.value = {
+            fileManager.deleteFile(it)
+            }
+    }
+
+
+    DeleteDialog(deletionDialog, delete_this)
+
     Column() {
         TopAppBar() {
             Text(text = "Song Select",
@@ -37,8 +50,11 @@ fun SongSelection(
             fileManager.songs.forEach {
                 item {
                     SongRow(
-                        SongHolder(it.name)
-                    ) { setSong(it) }
+                        song = SongHolder(it.name),
+                        selectSong = { setSong(it) },
+                        deleteSong = { deleteSong(it) }
+                    )
+
                 }
 
                 item {
@@ -49,24 +65,52 @@ fun SongSelection(
             }
         }
     }
-
-
 }
 
+@Composable
+fun DeleteDialog(
+    openDialog: MutableState<Boolean> = remember { mutableStateOf(false) },
+    onAccept: MutableState<() -> Unit> = remember { mutableStateOf({})}
+) {
+    Column() {
+        if (openDialog.value) { AlertDialog(
+            onDismissRequest = {
+                openDialog.value = false
+            }, title =  {
+                Text(text = "Deletion dialog")
+            }, text = {
+                Text(text = "Are you sure?")
+            }, confirmButton = {
+                Button(onClick = {
+                    openDialog.value = false
+                    onAccept.value()
+                }) {
+                    Text("Confirm")
+                }
+            }, dismissButton = {
+                Button(onClick = {
+                    openDialog.value = false
+                }) {
+                    Text("Reject")
+                }
+            }) }
+    }
+}
 
 @Preview
 @Composable
 fun SongRow(
     song: SongHolder = SongHolder(),
-    onClick: () -> Unit = {},
+    selectSong: () -> Unit = {},
+    deleteSong: () -> Unit = {},
 ) {
     Row(
-        modifier = Modifier.clickable(onClick = onClick),
+        modifier = Modifier.clickable(onClick = selectSong),
     )
     {
         Surface(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(0.8f)
                 .height(40.dp)
                 .padding(horizontal = 4.dp, vertical = 0.dp),
             shape = MaterialTheme.shapes.medium,
@@ -74,6 +118,12 @@ fun SongRow(
             Text( text = song.name,
                 style = MaterialTheme.typography.h5,
 
+            )
+        }
+        IconButton(onClick = deleteSong
+        ) {
+            Icon(
+                Icons.Filled.Delete, contentDescription = null
             )
         }
     }
